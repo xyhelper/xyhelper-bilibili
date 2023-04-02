c6cb9068d81cba82430aea4ad404061e9e617874
GCSLaoLi <lidong@xyhelper.cn>
2023-04-02 14:49:13 +0800

    为每个用户提供一个独立SESSION存储上下文

--- gpt/ask.go
+++ gpt/ask.go
@@ -6,21 +6,18 @@ import (
 	"time"
 
 	"github.com/gogf/gf/v2/frame/g"
-	"github.com/google/uuid"
 	"github.com/xyhelper/chatgpt-go"
 )
 
 // AskGPT ask gpt
-func AskGPT(ctx g.Ctx, msg string) error {
-	token := uuid.New().String()
+func AskGPT(ctx g.Ctx, msg string, token string, conversationID string, parentMessage string) (conversation_id string, parent_message string, err error) {
+	// token := uuid.New().String()
 	cli := chatgpt.NewClient(
 		// chatgpt.WithDebug(true),
 		chatgpt.WithTimeout(120*time.Second),
 		chatgpt.WithAccessToken(token),
 		chatgpt.WithBaseURI("https://freechat.lidong.xin"),
 	)
-	conversationID := ""
-	parentMessage := ""
 	stream, err := cli.GetChatStream(msg, conversationID, parentMessage)
 	if err != nil {
 		log.Fatalf("get chat stream failed: %v\n", err)
@@ -33,6 +30,9 @@ func AskGPT(ctx g.Ctx, msg string) error {
 
 		answer = text.Content
 
+		conversation_id = text.ConversationID
+		parent_message = text.MessageID
+
 	}
 
 	if stream.Err != nil {
@@ -40,6 +40,7 @@ func AskGPT(ctx g.Ctx, msg string) error {
 	}
 	// 输出换行
 	println()
-
-	return nil
+	println("conversation_id:", conversation_id)
+	println("parent_message:", parent_message)
+	return
 }
--- queue/queue.go
+++ queue/queue.go
@@ -5,6 +5,7 @@ import (
 	"xyhelper-bilibili/gpt"
 
 	"github.com/gogf/gf/v2/container/gqueue"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/util/gconv"
 )
@@ -16,16 +17,39 @@ type Ask struct {
 	Asker string
 }
 
+// Talk 会话
+type Talk struct {
+	ConversationID string `json:"conversation_id"` // 会话ID
+	ParentMessage  string `json:"parent_message"`  // 父消息
+}
+
+// Talks 会话列表
+var Talks = make(map[string]Talk)
+
 func QueueAnswer() {
 	for {
 		if v := Queue.Pop(); v != nil {
 			fmt.Println(" Pop:", v)
+			g.Dump(Talks)
 			ask := &Ask{}
 			gconv.Struct(v, ask)
 			ctx := gctx.New()
+			if _, ok := Talks[ask.Asker]; !ok {
+				// 不存在
+				Talks[ask.Asker] = Talk{}
+			}
+
 			println("-----------------------------")
 			println("回答用户:", ask.Asker, "的问题:", ask.Msg, "")
-			gpt.AskGPT(ctx, ask.Msg+"\n")
+			conversation_id, parent_message, err := gpt.AskGPT(ctx, ask.Msg+"\n", ask.Asker, Talks[ask.Asker].ConversationID, Talks[ask.Asker].ParentMessage)
+			if err != nil {
+				println("回答失败:", err)
+			} else {
+				Talks[ask.Asker] = Talk{
+					ConversationID: conversation_id,
+					ParentMessage:  parent_message,
+				}
+			}
 			println("-----------------------------")
 
 		} else {
